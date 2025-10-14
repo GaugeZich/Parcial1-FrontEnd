b49ff6f34516956b4a7b3486498b4263719c90cb
GaugeZich <guilleop.vale@gmail.com>
2025-10-14 19:01:01 -0300

    Eliminación de archivos anteriores para recuperatorio

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-    import CatalogoManager from '@/components/CatalogoManager.vue';
+
 </script>
 
 <template>
-    <CatalogoManager></CatalogoManager>
+
 </template>
 
 <style scoped></style>
